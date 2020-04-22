602c962bee46a48c6269053f77122cff8929af2d
Chris Price <christopher.r.price@gmail.com>
2020-04-21 22:59:41 -0400

    Set up the basic Core Data Stack.

---
+++ Tasks/Tasks/Model/Task+Convenience.swift
@@ -0,0 +1,29 @@
+//
+//  Task+Convenience.swift
+//  Tasks
+//
+//  Created by Chris Price on 4/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+// We need a way to initialize a Task object given its properties
+extension Task {
+    convenience init(identifier: UUID = UUID(),
+                     name: String,
+                     notes: String?,
+                     complete: Bool = false,
+                     context: NSManagedObjectContext) {
+        
+        // Set up the NSManagedObject portion of the Task object
+        self.init(context: context)
+        
+        // Assign out unique values to the attributes we creatied in the data model file.
+        self.identifier = identifier
+        self.name = name
+        self.notes = notes
+        self.complete = complete
+    }
+}
